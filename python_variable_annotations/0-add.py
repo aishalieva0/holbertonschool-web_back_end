c743d6ce56568cba68090d5a62c5d668bdcb21c3
aishalieva0 <aysalieva6@gmail.com>
2024-04-29 23:08:34 +0400

    Function add  documented

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -5,4 +5,13 @@ and returns their sum as a float"""
 
 
 def add(a: float, b: float) -> float:
+    """_summary_
+
+    Args:
+        a (float): _description_
+        b (float): _description_
+
+    Returns:
+        float: _description_
+    """
     return a + b
