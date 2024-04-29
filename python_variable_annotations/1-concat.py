dfa4242cea9fb0f2abcd8d810ad19080df57c775
aishalieva0 <aysalieva6@gmail.com>
2024-04-29 23:23:48 +0400

    feat: Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""takes a string str1 and a string str2 as arguments
+returns a concatenated string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """takes a string str1 and a string str2 as arguments"""
+    return str1 + str2
