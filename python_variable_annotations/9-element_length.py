3fea3053e286a2a37cdd1667e9ab005322478056
aishalieva0 <aysalieva6@gmail.com>
2024-04-30 00:50:30 +0400

    added docstring

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -6,5 +6,5 @@ from typing import Iterable, Sequence, Tuple, List
 
 def element_length(lst: Iterable[Sequence]) -> List[
         Tuple[Sequence, int]]:
-    """ """
+    """Calculate the length of each element in iterable"""
     return [(i, len(i)) for i in lst]
