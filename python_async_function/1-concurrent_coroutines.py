f3614cb2e23c6f08bcb4f4815673fab07cdab826
aishalieva0 <aysalieva6@gmail.com>
2024-04-30 18:12:51 +0400

    fix:annotated

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """asynchronous coroutine"""
+from typing import List
+
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """return the list of all the delays"""
     all_delays = []
     for _ in range(n):
         all_delays.append(await wait_random(max_delay))
-    return all_delays
+    return sorted(all_delays)
