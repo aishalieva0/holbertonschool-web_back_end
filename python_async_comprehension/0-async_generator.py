78434562b1af9e8fb7a400eaacc0c418f8a52419
aishalieva0 <aysalieva6@gmail.com>
2024-04-30 19:19:49 +0400

    feat:Async Generator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Async Generator"""
+
+import random
+import asyncio
+
+
+async def async_generator():
+    """yield a random number between 0 and 10"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(1, 10)
