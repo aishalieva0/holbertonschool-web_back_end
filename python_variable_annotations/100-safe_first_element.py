df7fd8223ee5cea112eed4a18a601d1a59f8988a
aishalieva0 <aysalieva6@gmail.com>
2024-05-01 18:13:03 +0400

    fix

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Duck typing"""
 
-from typing import Union, Sequence, Any
+from typing import Union, Sequence, Any, Optional
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
     """first element of a sequence"""
     if lst:
         return lst[0]
