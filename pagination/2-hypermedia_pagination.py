5aeee6f1a54fcd286020148a65bb8316a1059a7f
aishalieva0 <aysalieva6@gmail.com>
2024-05-10 00:48:11 +0400

    Hypermedia pagination

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+"""Hypermedia pagination"""
+
+import csv
+import math
+from typing import List, Dict
+
+index_range = __import__("0-simple_helper_function").index_range
+# get_page = __import__("1-simple_pagination").get_page
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """returns page of the dataset"""
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+
+        range = index_range(page, page_size)
+        start = range[0]
+        end = range[1]
+        try:
+            return self.dataset()[start:end]
+        except IndexError:
+            []
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """
+        Args:
+            page (int, optional): Defaults to 1.
+            page_size (int, optional): Defaults to 10.
+
+        Returns:
+            Dict: containing the following key-value pairs:
+            page_size: the length of the returned dataset page
+            page: the current page number
+            data: the dataset page (equivalent to return from previous task)
+            next_page: number of the next page, None if no next page
+            prev_page: number of the previous page, None if no previous page
+            total_pages: total number of pages in the dataset as an integer
+        """
+
+        data = self.get_page(page, page_size)  # data per page
+
+        next_page = page + 1
+
+        prev_page = page - 1 if page > 1 else None
+
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+
+        if not data:
+            page_size = 0
+            next_page = 0
+
+        return {
+            "page_size": page_size,
+            "page": page,
+            "data": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages,
+        }
