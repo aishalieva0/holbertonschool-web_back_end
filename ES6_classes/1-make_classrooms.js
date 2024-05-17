633601438007a01e271f38176712608f67985f78
aishalieva0 <aysalieva6@gmail.com>
2024-05-17 14:45:49 +0400

    eslint-disable

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import ClassRoom from "./0-classroom.js";
 export default function initializeRooms() {
     const clsRoom = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
